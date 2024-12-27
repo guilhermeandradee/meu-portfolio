b0e28f5d41a7e0a5710901746e4691492af14c3d
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-12-27 10:20:33 -0300

    add livechat, exclude header

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     return(
         <>
             <section className="font-ralewey row d-flex justify-content-center w-100 px-3 px-md-0 mt-3">
-                <div onClick={() => openInNewTab('https://github.com/guilhermeandradee/Cougar')} className="col-md-5 col-12 me-md-3 rounded-lg image-cougar px-5 mb-3 preencher-div" >
+                <div onClick={() => openInNewTab('https://cougar-two.vercel.app/')} className="col-md-5 col-12 me-md-3 rounded-lg image-cougar px-5 mb-3 preencher-div" >
                     <div className="elemento-de-dentro w-100  p-4 bg-cougar ">
                         <h2 className="fw-bold">Cougar</h2>
 
@@ -18,7 +18,7 @@ const Projects = () => {
                     </div>
                 </div>
 
-                <div onClick={() => openInNewTab('https://github.com/guilhermeandradee/future-bank')} className="col-md-5 col-12 rounded-lg image-future-bank px-5 mb-3 preencher-div" >
+                <div onClick={() => openInNewTab('https://future-bank.vercel.app/')} className="col-md-5 col-12 rounded-lg image-future-bank px-5 mb-3 preencher-div" >
                     <div className="elemento-de-dentro w-100  p-4 bg-cougar">
                         <h2 className="fw-bold">Future Bank</h2>
 
@@ -29,7 +29,7 @@ const Projects = () => {
                     </div>
                 </div>
 
-                <div onClick={() => openInNewTab('https://github.com/guilhermeandradee/Cougar')} className="col-md-5 col-12 me-md-3 rounded-lg image-livechat px-5 mb-3 preencher-div" >
+                <div onClick={() => openInNewTab('https://live-chat-smoky.vercel.app/')} className="col-md-5 col-12 me-md-3 rounded-lg image-livechat px-5 mb-3 preencher-div" >
                     <div className="elemento-de-dentro w-100  p-4 bg-cougar ">
                         <h2 className="fw-bold">Livechat</h2>
 
